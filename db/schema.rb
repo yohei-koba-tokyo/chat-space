2bbff09c27a7aa68666707f9d93debd0fa82b57e
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-02-10 14:49:03 +0900

    user registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 20200210031912) do
+=======
+ActiveRecord::Schema.define(version: 20200210012951) do
+>>>>>>> Stashed changes
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
